5c42eaf9eec58a33a8649e926bc75797feecaa59
mbecel <mael1.becel@epitech.eu>
2022-02-11 01:21:54 +0100

    refactor (mouli.sh): Division of tests in 3 parts norme

--- .build/mouli.sh
+++ .build/mouli.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 BOLD="\e[1m"
 UNDERLINE="\e[4m"
 DEFAULT="\e[0m"
@@ -41,64 +43,9 @@ showbar() {
     echo -e " $DEFAULT$BOLD$percentage$DEFAULT%"
 }
 
-bloc() {
-    echo -e " $ORANGE$BOLD$UNDERLINE$1$DEFAULT\n"
-}
-
-end() {
-    echo ""
-    echo -e " \c"
-    showbar $BLOC_PASSED $BLOC_TOTAL 20
-    echo -e "\n ========================\n"
-    BLOC_TOTAL=0
-    BLOC_PASSED=0
-    BLOC_FAILED=0
-}
-
-mouli() {
-    TOTAL=$((TOTAL+1))
-    BLOC_TOTAL=$((BLOC_TOTAL+1))
-    echo -e "$BOLD    Test $BLOC_TOTAL :'$1' $DEFAULT"
-    $2 > /dev/null
-    retrn=$?
-    if [ "$retrn" != "$4" ]; then
-        FAILED=$((FAILED+1))
-        BLOC_FAILED=$((BLOC_FAILED+1))
-        echo -e "$BOLD$RED        [KO]$DEFAULT Bad return, need '$4' but have '$retrn'"
-    else
-        RES=$($2)
-        if [ "$RES" = "$3" ]; then
-            PASSED=$((PASSED+1))
-            BLOC_PASSED=$((BLOC_PASSED+1))
-            echo -e "$BOLD$GREEN        [OK]$DEFAULT Test PASSED"
-        else
-            FAILED=$((FAILED+1))
-            BLOC_FAILED=$((BLOC_FAILED+1))
-            echo -e "$BOLD$RED        [KO]$DEFAULT Bad output, need '$3' but have '$RES'"
-        fi
-    fi
-    echo ""
-}
-
-displayres() {
-    showbar $PASSED $TOTAL 33
-    echo  ""
-    echo -e "$PURPLE TOTAL $BOLD$TOTAL$DEFAULT |$GREEN PASSED $BOLD$PASSED$DEFAULT |$RED FAILED $BOLD$FAILED$DEFAULT"
-    echo -e "$BOLD ====================================$DEFAULT"
-}
 
 ############# INIT ##########
 
-TOTAL=0
-PASSED=0
-FAILED=0
-
-BLOC_TOTAL=0
-BLOC_PASSED=0
-BLOC_FAILED=0
-
-make re > /dev/null
-make clean > /dev/null
 
 if [ "$1" = "-n" ]; then
     NORME=$(normez -i)
@@ -136,10 +83,17 @@ echo -e "\n ========================\n"
 
 ############# Tests ##########
 
-# mouli Test_name Command Resultat $?
+if [[ ! -z $(ls -alf tests/ | grep ".unit") ]]; then
+    echo -e "\n        $BOLD========================$DEFAULT"
+    echo -e "        $PURPLE$BOLD      $UNDERLINE Units tests. $DEFAULT"
+    echo -e "        $BOLD========================$DEFAULT"
+    ./unit
+fi
 
-eval $(cat tests/*.mouli)
+if [[ ! -z $(ls -alf tests/ | grep ".fonc") ]]; then
+    echo -e "\n        $BOLD========================$DEFAULT"
+    echo -e "        $PURPLE$BOLD  $UNDERLINE Fonctionnal tests. $DEFAULT"
+    echo -e "        $BOLD========================$DEFAULT"
+    ./fonc
+fi
 
-############# Display #########
-
-displayres
